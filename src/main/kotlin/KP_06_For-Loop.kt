f46b4f7b0bbc7963292ac43217b23c4e843c64c5
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-20 09:23:56 +0800

    KP_06_For-Loop.kt

---
+++ src/main/kotlin/KP_06_For-Loop.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //For Loop
+
+    for(number in 1 .. 5){
+        println(number)
+    }
+    println()
+    for(letters in 'B' .. 'X'){
+        println(letters)
+    }
+
+}
+
+//Source: https://www.youtube.com/watch?v=gc-fbBzdOHY
+//https://www.geeksforgeeks.org/kotlin-for-loop/?ref=gcse
