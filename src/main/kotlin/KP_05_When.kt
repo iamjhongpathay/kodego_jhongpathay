7d89291af8733bf7743455bf67c6c3799e942c37
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-20 09:22:16 +0800

    KP_05_When.kt

---
+++ src/main/kotlin/KP_05_When.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //When Statement
+    var itemNumber : Int = 2
+    when(itemNumber){
+        1 -> print("Clothes")    //Statement 1
+        2 -> print("Food")       //Statement 2
+        3 -> print("Toiletries") //Statement 3
+        else -> println("Invalid Item Number") //Default Statement
+    }
+}
+
+//Source: https://www.youtube.com/watch?v=gc-fbBzdOHY
+//https://www.geeksforgeeks.org/kotlin-when-expression/
