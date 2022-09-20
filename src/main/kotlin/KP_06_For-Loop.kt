2e6c360b459e0f897173b1d58272e915661d722a
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-20 10:58:17 +0800

    KP_06_For-Loop - 2nd commit

--- src/main/kotlin/KP_06_For-Loop.kt
+++ src/main/kotlin/KP_06_For-Loop.kt
@@ -9,7 +9,15 @@ fun main(){
         println(letters)
     }
 
+    //Accessing the String Elements
+    var string = "Jefferson"
+    for(i in string.indices){
+        print("${string[i]} ")
+    }
+
+
 }
 
 //Source: https://www.youtube.com/watch?v=gc-fbBzdOHY
-//https://www.geeksforgeeks.org/kotlin-for-loop/?ref=gcse
+        //https://www.geeksforgeeks.org/kotlin-for-loop/?ref=gcse
+        //https://www.geeksforgeeks.org/kotlin-string/?ref=gcse
