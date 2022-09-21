46fffa21efbcd93ef6803ecedab1d6193129a76b
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-21 15:46:33 +0800

    KP_07_Repeat - 1st commit

---
+++ src/main/kotlin/KP_07_Repeat.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //Repeat statement
+
+    repeat(5){
+        println("Hello World!")
+    }
+
+    repeat(5){ctr ->
+       println("Hello World! $ctr")
+    }
+}
+
+//Source: https://www.youtube.com/watch?v=Jhv1RcNIDxs
+
