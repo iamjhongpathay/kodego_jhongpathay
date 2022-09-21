352b9a4d01d59d1d99fa65e13ba1bdabf99854ab
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-21 16:36:51 +0800

    KP_08_While-Loop - 1st commit

---
+++ src/main/kotlin/KP_08_While-Loop.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //While Loop
+
+    var ctr : Int = 1
+    while (ctr <= 5){ //condition
+        println("Hello World! $ctr")
+
+        ctr++ //update
+    }
+}
+
+//Source: https://www.youtube.com/watch?v=Jhv1RcNIDxs
+        //https://www.geeksforgeeks.org/kotlin-while-loop/?ref=gcse
+        //https://www.programiz.com/kotlin-programming/while-loop
