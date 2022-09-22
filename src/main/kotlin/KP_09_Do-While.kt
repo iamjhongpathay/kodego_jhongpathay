1fdd282c03ab434f0e1bcbc40867de27637ac6fc
Jefferson Pathay <jeffersonpathay@gmail.com>
2022-09-22 11:09:44 +0800

    KP_09_Do-While - 1st commit

---
+++ src/main/kotlin/KP_09_Do-While.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //do while loop
+    var ctr : Int = 1
+    do{
+        println("Hello World!")
+
+        ctr++ //update
+    }while(ctr <= 5) //condition
+
+
+}
+
+//Source: https://www.youtube.com/watch?v=Jhv1RcNIDxs
+        //https://www.geeksforgeeks.org/kotlin-do-while-loop/?ref=gcse
+        //https://www.programiz.com/kotlin-programming/while-loop
